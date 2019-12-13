fb884f7cea23db7ab0ac1c17451271a74bd07754
Ashok Pagatipalli <ashok.pagatipalli@ad.infosys.com>
2019-12-13 17:19:07 +0530

    Updates

---
+++ precache-manifest.5863f509795991abfcf8629f75671ce4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01a79f513198473240a8",
+    "url": "/me/static/css/main.64e4aae7.chunk.css"
+  },
+  {
+    "revision": "01a79f513198473240a8",
+    "url": "/me/static/js/main.01a79f51.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "df893a9066c28998baa6f12ea6dfbd25",
+    "url": "/me/index.html"
+  }
+];
